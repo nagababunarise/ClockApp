5d18e5b03a774cd6f0d50216f21c1febac8a7fef
nagababunarise <nagababunarise123@gmail.com>
2023-11-14 05:30:36 +0000

    clock

---
+++ src/components/Clock/index.js
@@ -0,0 +1,30 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  state = {date: new Date()}
+
+  componentDidMount() {
+    this.timerId = setInterval(this.tick, 1000)
+  }
+
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerId)
+  }
+
+  render() {
+    const {date} = this.state
+    console.log(date)
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
